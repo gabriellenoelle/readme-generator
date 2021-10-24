53dc0203ccd840c004f5f2fed6f5260caf33d2c0
gabriellenoelle <88753098+gabriellenoelle@users.noreply.github.com>
2021-10-24 01:12:02 -0400

    installed inquirer npm

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const readmeDataArgs = process.argv.slice(2, process.argv.length);
+const [project, github] = readmeDataArgs;
 console.log(readmeDataArgs);
 
 const generateReadme = (projectName, githubName) =>
-  `Project: ${projectName}
-Github: ${githubName}`;
-console.log(generateReadme("Readme Generator", "gabriellenoelle"));
+  `
+Project: ${projectName}
+Github: ${githubName}
+`;
+console.log(project, github);
+console.log(generateReadme(project, github));
